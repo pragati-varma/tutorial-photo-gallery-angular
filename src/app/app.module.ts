ed3a7c25c90ad1ed4e3013e6887ab32cb89865f3
pragati-varma <pverma@zolute.consulting>
2022-11-10 23:33:40 +0530

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,6 @@ import { environment } from '../environments/environment';
             // Register the ServiceWorker as soon as the application is stable
             // or after 30 seconds (whichever comes first).
             registrationStrategy: 'registerWhenStable:30000'
-        }), ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: environment.production,
-            // Register the ServiceWorker as soon as the app is stable
-            // or after 30 seconds (whichever comes first).
-            registrationStrategy: 'registerWhenStable:30000'
         })],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
     bootstrap: [AppComponent]
