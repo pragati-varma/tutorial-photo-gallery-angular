929d03c928e466b4266ac6f63335df7ad33866f6
pragati-varma <pverma@zolute.consulting>
2022-11-11 18:19:29 +0530

    login for valiation

--- src/app/AuthLayout/login/login.page.ts
+++ src/app/AuthLayout/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,31 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  loginForm: FormGroup;
+  isSubmitted = false;
   constructor(
-    private navCtrl: NavController
-  ) { }
+    private navCtrl: NavController,
+    public formBuilder: FormBuilder
+  ) { 
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+   })
+  }
 
   ngOnInit() {
   }
   loginUser(){
-    console.log('emter email')
+    console.log('Please put required values!')
+    this.isSubmitted = true;
+    if (!this.loginForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.loginForm.value)
     this.navCtrl.navigateForward(['otp']);
+    }
+  }
+  get errorControl() {
+    return this.loginForm.controls;
   }
 }
